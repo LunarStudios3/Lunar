7ba09bd77770117d55f7c30768fe6d2d2be0be7d
Lunar <lunarstudios3@gmail.com>
2021-09-04 13:23:31 -0400

    added landing movie and logo

--- src/components/Landing.js
+++ src/components/Landing.js
@@ -1,12 +1,19 @@
 import Container from 'react-bootstrap/Container'
 import '../styles/landing.css'
+import Video from '../assets/LunarLanding.mp4'
 
 function Landing (){
     return(
-  <Container fluid='lg' className='starVideo'>
-
-  </Container>
-);
+        <Container fluid="lg" id="Video">
+            <video loop muted autoPlay className="backgroundVideo">
+                <source src={Video} type="video/mp4" />
+                {console.log("Hello")}
+            </video> 
+            
+                <img className="landingLogo" src="https://i.imgur.com/3KlQtZ7.png" alt="Lunar Studios"/>
+            
+        </Container>
+    );
 }
 
 export default Landing
